efa5ad0c86a467fb144186a6683089c77c9e61d2
Tonmoy Roy <rxdevs100@gmail.com>
2022-10-03 12:19:27 +0600

    errors solved & frontend updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'tw-elements';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 reportWebVitals();
