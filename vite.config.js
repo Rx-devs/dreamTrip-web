7bf3d0b51c9a66e5d1fc2859fa28a6c94d62e16e
tonmoy-dev <rxdevs100@gmail.com>
2024-03-31 17:53:55 +0600

    chore(devdeps): add or update dev dependency packages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true
+    port: 5000,
+    host: true,
   },
-})
+});
 
 // npm run dev -- --host
