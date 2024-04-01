20c0e3e6546aa3e92125492fe5550d79d4feb43f
tonmoy-dev <rxdevs100@gmail.com>
2024-04-01 21:32:39 +0600

    feat(navigation): update navigation features in navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,5 +1,11 @@
-import { RiDashboardLine, RiLogoutBoxLine } from "@remixicon/react";
-import { NavLink } from "react-router-dom";
+import {
+  RiCloseLine,
+  RiDashboardLine,
+  RiLogoutBoxLine,
+  RiMenu2Line,
+} from "@remixicon/react";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
 let activeStyle = {
@@ -13,52 +19,75 @@ let activeStyle2 = {
 };
 
 export default function Navbar() {
+  const [showNavLinks, setShowNavLinks] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+
   const { user, logout } = useAuth();
   return (
     <nav className="navbar bg-base-100 px-0">
+      {/* logo and navigation for small screen */}
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex="0" role="button" className="btn btn-link lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-
-          <ul
-            tabIndex="0"
-            className="menu menu-sm gap-1 dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-lg w-screen scale-0"
+          {/* menu button */}
+          <div
+            onClick={() => {
+              setShowNavLinks(!showNavLinks);
+              setShowDropdown(false);
+            }}
+            tabIndex={1}
+            role="button"
+            className="btn btn-link lg:hidden"
           >
-            {/* nav links for small screen */}
-            {navItems.map((item) => (
-              <li key={item.name}>
-                <NavLink
-                  style={({ isActive }) =>
-                    isActive ? activeStyle2 : undefined
-                  }
-                  className="rounded-lg py-2"
-                  to={item.path}
-                >
-                  {item.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+            {!showNavLinks ? (
+              <RiMenu2Line size={22} color="#3fd0d4" />
+            ) : (
+              <RiCloseLine
+                size={22}
+                color="#3fd0d4"
+                className="bg-neutral-900"
+              />
+            )}
+          </div>
+          {/* nav links for small screen */}
+          {showNavLinks && (
+            <ul
+              tabIndex={1}
+              className="menu menu-sm gap-1 dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-b-lg w-screen scale-0 lg:hidden "
+            >
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <NavLink
+                    style={({ isActive }) =>
+                      isActive ? activeStyle2 : undefined
+                    }
+                    className="rounded-lg py-2"
+                    to={item.path}
+                    onClick={() => {
+                      setShowNavLinks(false);
+                      setShowDropdown(false);
+                    }}
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-        <a className="btn btn-link no-underline hover:no-underline text-2xl md:text-3xl font-semibold md:ms-4 text-[#3fd0d4]">
+        {/* logo */}
+        <Link
+          style={{ color: "#3fd0d4" }}
+          to="/"
+          onClick={() => {
+            setShowDropdown(false);
+            setShowNavLinks(false);
+          }}
+          className="btn btn-link no-underline hover:no-underline text-2xl md:text-3xl font-semibold md:ms-6"
+        >
           dreamTrip
-        </a>
+        </Link>
       </div>
+      {/* navigation for large screen */}
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           {/* nav links for large screen */}
@@ -67,6 +96,7 @@ export default function Navbar() {
               <NavLink
                 style={({ isActive }) => (isActive ? activeStyle : undefined)}
                 to={item.path}
+                onClick={() => setShowDropdown(false)}
                 className="btn btn-link no-underline text-base hover:no-underline active:no-underline hover:text-[#3fd0d4]"
               >
                 {item.name}
@@ -80,35 +110,54 @@ export default function Navbar() {
         {user.email ? (
           <div className="dropdown dropdown-end cursor-pointer">
             {/* avatar */}
-            <div className="ml-3 relative m-1" tabIndex="0" role="button">
+            <div
+              onClick={() => setShowDropdown(!showDropdown)}
+              className="ml-3 relative m-1"
+              tabIndex="0"
+              role="button"
+            >
               <div className="avatar">
                 <div className="w-8 rounded-full ring ring-[#3fd0d4] ring-offset-base-100 ring-offset-2">
                   <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                 </div>
               </div>
             </div>
-            <ul
-              tabIndex="0"
-              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52 text-base mt-3 md:mt-5"
-            >
-              <li>
-                <NavLink className="rounded-lg" to="/dashboard">
-                  <RiDashboardLine size={20} />
-                  Dashboard
-                </NavLink>
-              </li>
-              <li>
-                <a className="rounded-lg" onClick={logout}>
-                  <RiLogoutBoxLine size={20} />
-                  Logout
-                </a>
-              </li>
-            </ul>
+            {/* dropdown items */}
+            {showDropdown && (
+              <ul
+                tabIndex="0"
+                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52 text-base mt-3 md:mt-5"
+              >
+                <li>
+                  <NavLink
+                    onClick={() => setShowDropdown(false)}
+                    className="rounded-lg"
+                    to="/dashboard"
+                  >
+                    <RiDashboardLine size={20} />
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li>
+                  <a
+                    className="rounded-lg"
+                    onClick={() => {
+                      logout();
+                      setShowDropdown(false);
+                    }}
+                  >
+                    <RiLogoutBoxLine size={20} />
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            )}
           </div>
         ) : (
           <NavLink
             className="btn btn-link no-underline text-base hover:no-underline active:no-underline hover:text-[#3fd0d4]"
             to="/login"
+            onClick={() => setShowNavLinks(false)}
           >
             Join Now
           </NavLink>
@@ -119,7 +168,7 @@ export default function Navbar() {
 }
 
 const navItems = [
-  { name: "Home", path: "/home" },
+  { name: "Home", path: "/" },
   { name: "Gallery", path: "/gallery" },
   { name: "Destinations", path: "/destinations" },
   { name: "Blogs", path: "/blogs" },
